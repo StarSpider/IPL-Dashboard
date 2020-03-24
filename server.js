539be048a545d7db05dae7e3631e58ece72f3645
Baibhav Ojha <36107094+StarSpider@users.noreply.github.com>
2020-03-25 02:01:25 +0530

    deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // serves files from our dist directory which now contains out index.html file
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log("Listening on port: " + port);
